dca17832ef30232ef82764150f0088ee94cdec3b
Sourav Kumar <souravsemail2001@gmail.com>
2023-08-02 16:24:41 +0530

    deleted the client side

--- client/src/App.jsx
+++
@@ -1,3 +0,0 @@
-export default function App() {
-  return <h1>Hello this is the frontend of the app</h1>;
-}
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 const todos = require("./todos");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.json(todos);
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ allTodos: todos });
 });
 app.listen(process.env.PORT || PORT, (req, res) => {
   console.log("The server has started on port 8000!");
