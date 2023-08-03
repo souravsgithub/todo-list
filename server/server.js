021f4da44f555582020a5c972a24ae0bc87dae1b
Sourav Kumar <souravsemail2001@gmail.com>
2023-08-03 13:51:36 +0530

    gave the adding and retrieving todos functionality from the db

--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
-const todos = require("./todos");
 const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
 
-app.use(cors());
+// I thhink mongoDB does not support the callback style anymore
 
-app.get("/api", (req, res) => {
-  res.json({ allTodos: todos });
-});
-app.listen(process.env.PORT || PORT, (req, res) => {
-  console.log("The server has started on port 8000!");
-});
+MongoClient.connect(
+  "mongodb+srv://souravsusername:souravspassword@mycluster.zuo6jdo.mongodb.net/?retryWrites=true&w=majority"
+)
+  .then((client) => {
+    console.log("Connected to the database!");
+    const db = client.db("todo-list");
+    const todosCollection = db.collection("todos-collection");
+
+    app.set("view-engine", "ejs");
+    app.use(cors());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    app.get("/", (req, res) => {
+      todosCollection
+        .find()
+        .toArray()
+        .then((todos) => {
+          res.render("index.ejs", { todosArr: todos });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+
+    app.post("/addTodo", (req, res) => {
+      const todoName = req.body["todo-item"];
+      todosCollection.insertOne({
+        todoName,
+        isComplete: false,
+        isDeleted: false,
+      });
+      res.redirect("/");
+    });
+
+    app.listen(process.env.PORT || PORT, (req, res) => {
+      console.log("The server has started on port 8000!");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
